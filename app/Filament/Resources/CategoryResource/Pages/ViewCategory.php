ae5ee4c26346104c5974709ac048a3288b85e056
ShiranaiZo <faishalbariqmaulana123@gmail.com>
2024-03-21 17:20:26 +0700

    feat: [FM] master categories

--- app/Filament/Resources/CategoryResource/Pages/ViewCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/ViewCategory.php
@@ -14,6 +14,7 @@ class ViewCategory extends ViewRecord
     {
         return [
             Actions\EditAction::make(),
+            Actions\DeleteAction::make(),
         ];
     }
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,11 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->colors([
                 'primary' => "#0EA5E9",
+                'danger' => "#FF6347",
+                'gray' => "#808080",
+                'info' => "#5bc0de",
+                'success' => "#5cb85c",
+                'warning' => "#FFD700",
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
